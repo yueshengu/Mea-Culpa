05b240e30bd7a19d8b8d65b3df1911990cca11d3
Josh Dillon <joshdillon29@gmail.com>
2016-04-24 14:34:00 -0400

    Pushing emotion analysis

--- lib/professor_analysis_josh.R
+++ lib/professor_analysis_josh.R
@@ -182,3 +182,68 @@ output$sentiment_bar_chart2 <- renderPlot({
     scale_fill_manual(values=c("#990000", "#003399", "#339933"))
 
 })
+
+########################## Emotions ##################################
+
+source("classify_emotion.R")
+source("classify_polarity.R")
+source("create_matrix.R")
+
+# classify emotion
+class_emo = classify_emotion(combined$workload_text, algorithm="bayes", prior=1.0)
+# get emotion best fit
+emotion = class_emo[,7]
+# substitute NA's by "unknown"
+emotion[is.na(emotion)] = "unknown"
+
+# classify polarity
+class_pol = classify_polarity(combined$workload_text, algorithm="bayes")
+# get polarity best fit
+polarity = class_pol[,4]
+
+# data frame with results
+sent_df = data.frame(text=combined$workload_text, emotion=emotion,
+polarity=polarity, stringsAsFactors=FALSE)
+
+# sort data frame
+sent_df = within(sent_df,
+  emotion <- factor(emotion, levels=names(sort(table(emotion), decreasing=TRUE))))
+
+#####
+
+# plot distribution of emotions
+ggplot(sent_df, aes(x=emotion)) +
+geom_bar(aes(y=..count.., fill=emotion)) +
+scale_fill_brewer(palette="Dark2") +
+labs(x="emotion categories", y="number of tweets")
+
+# plot distribution of polarity
+ggplot(sent_df, aes(x=polarity)) +
+geom_bar(aes(y=..count.., fill=polarity)) +
+scale_fill_brewer(palette="RdGy") +
+labs(x="polarity categories", y="number of tweets")
+
+# separating text by emotion
+emos = levels(factor(sent_df$emotion))
+nemo = length(emos)
+emo.docs = rep("", nemo)
+for (i in 1:nemo)
+{
+   tmp = combined$workload_text[emotion == emos[i]]
+   emo.docs[i] = paste(tmp, collapse=" ")
+}
+
+# remove stopwords
+emo.docs = removeWords(emo.docs, stopwords("english"))
+emo.docs = removeWords(emo.docs, c("group", "final.", "final", "disgusting"))
+# create corpus
+corpus = Corpus(VectorSource(emo.docs))
+tdm = TermDocumentMatrix(corpus)
+tdm = as.matrix(tdm)
+colnames(tdm) = emos
+
+# comparison word cloud
+comparison.cloud(tdm, colors = brewer.pal(nemo, "Dark2"),
+   scale = c(2,.5), random.order = FALSE, title.size = 1.5)
+
+
